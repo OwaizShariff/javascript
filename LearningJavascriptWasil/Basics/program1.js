552b5a7e8a520bf98016ed344b0ac5c0c1cb3038
wasil <mohammedwasil121@gmail.com>
2024-12-23 07:17:50 +0530

    Javscript basics

---
+++ LearningJavascriptWasil/Basics/program1.js
@@ -0,0 +1,16 @@
+function greet() {
+    alert("Iam Learning javascript");
+}
+
+
+
+function personal(){
+    // Use console.log to print the information in a readable format
+    console.log("Personal Information: \n" +
+       "Name: " + name + "\n" +
+       "Age: " + Age + "\n" +
+       "Qualification: " + Qualification + "\n" +
+       "Date of Birth: " + DateOfBirth + "\n" +
+       "Nationality: " + Nationality);
+      }
+
