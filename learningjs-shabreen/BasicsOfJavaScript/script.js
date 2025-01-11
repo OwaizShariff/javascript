540401cc54907b9d65d03b09a54c3a1981c8f374
Shabreen <shabreenfirdose12@gmail.com>
2025-01-11 07:13:29 +0530

    Arrays

---
+++ learningjs-shabreen/BasicsOfJavaScript/script.js
@@ -0,0 +1,9 @@
+function abc(subject){
+    console.log("The subject iam learning now is")
+}
+
+abc("Javascript")
+
+const pqr = { name:"shabreen", age: 21, city:"Mysore", iamastudent:true}
+
+ console.log("Pqr", pqr.name, pqr.age,pqr.city,pqr.iamastudent)
